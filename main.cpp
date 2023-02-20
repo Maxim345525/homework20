dc7b0f2747607fde8435c6dc747f3bf5438dd676
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-20 20:12:07 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
-a
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 13;
+	int b = 33;
+	int* x_ptr = &a;
+	int* b_ptr = &b;
+	cout << "number->" << endl;
+	cout << *x_ptr << endl;
+	cout << *b_ptr << endl;
+	if (a > b)
+	{
+		cout << "Max number->" << a << endl;
+	}
+	else(a < b);
+	{
+		cout << "Max number->" << b << endl;
+	}
+}
