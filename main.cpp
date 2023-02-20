b1e76215c316d3dcba33bbd9b0a63339dece8a20
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-20 20:49:30 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
-a
+
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Enter a: ";
+    cin >> a;
+    int* x_ptr = &a;
+    cout << "Enter b: ";
+    cin >> b;
+    int* b_ptr = &b;
+    if (a > b) swap(a, b);
+    if (a > b) swap(a, b);
+    cout << ' ' << *x_ptr << ' ' << *b_ptr << ' ' << '\n';
+    system("pause");
+    return 0;
+}
+
