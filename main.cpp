1e07ed6f71b4499e482311a8077c4fb5caa1c871
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-20 20:30:20 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
-a
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout << "Input znak->";
+	cin >> a;
+	int* x_ptr = &a;
+	if (a > 0 || a == 0)
+	{
+		cout << *x_ptr << "  Znak +" << endl;
+		return 0;
+	}
+	else(a < 0);
+	{
+		cout << *x_ptr << "   Znak -" << endl;
+	}
+}
