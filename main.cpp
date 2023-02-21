990272d4d7d98ff61e35a89253608fdccebadb19
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-21 19:01:58 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
-a
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int* x_ptr = &n;
+    cout << "Enter n: ";
+    cin >> n;
+    int* Arr;
+    Arr = new int[n];
+    cout << "Array:" << endl;
+    for (int i = 0; i < n; i++) cin >> *(Arr + i);
+    int sum = 0;
+    for (int i = 0 + 1; i < n; i++) 
+    {
+        sum += Arr[i];
+    }
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
