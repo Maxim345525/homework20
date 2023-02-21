05c826880776419fa270a2c7a55da1b9d8412a65
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-21 18:53:10 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,29 @@
-a
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, d;
+    cout << "Input number a->";
+    cin >> a;
+    cout << "Input number b->";
+    cin >> b;
+    cout << "Input znak d->";
+    cin >> d;
+    if (d == ' + ' )
+    {
+        cout << a + b << endl;
+        return 0;
+    }
+    if (d == '-')
+    {
+        cout << a - b << endl;
+    }
+    if (d == '*')
+    {
+        cout << a * b << endl;
+    }
+    if (d == '/')
+    {
+        cout << a / b << endl;
+    }
+}
